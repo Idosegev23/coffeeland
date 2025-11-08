f133d58d5092b73c4cb7d6bf82421d3e800bcaeb
Idosegev23 <idosegev23@gmail.com>
2025-11-08 22:18:55 +0200

    Use real venue images in MagicBento gallery

--- app/(public)/page.tsx
+++ app/(public)/page.tsx
@@ -27,42 +27,42 @@ export default function HomePage() {
             cards={[
               {
                 id: '1',
-                image: 'https://images.unsplash.com/photo-1503454537195-1dcabb73ffb9?w=800&h=600&fit=crop',
+                image: '/images/untitled-1.jpg',
                 label: 'משחקייה',
                 title: 'מתקני משחק מגוונים',
                 description: 'מגלשות, טרמפולינות ופינות משחק מאובטחות לכל הגילאים'
               },
               {
                 id: '2',
-                image: 'https://images.unsplash.com/photo-1530103862676-de8c9debad1d?w=800&h=600&fit=crop',
+                image: '/images/untitled-15.jpg',
                 label: 'סדנאות',
                 title: 'יצירה והנאה',
                 description: 'סדנאות יצירה מגוונות לילדים בכל גיל'
               },
               {
                 id: '3',
-                image: 'https://images.unsplash.com/photo-1509062522246-3755977927d7?w=800&h=600&fit=crop',
+                image: '/images/untitled-25.jpg',
                 label: 'ימי הולדת',
                 title: 'חגיגות בלתי נשכחות',
                 description: 'חבילות מלאות לימי הולדת עם אירוח חם ומקצועי'
               },
               {
                 id: '4',
-                image: 'https://images.unsplash.com/photo-1544025162-d76694265947?w=800&h=600&fit=crop',
+                image: '/images/untitled-40.jpg',
                 label: 'בית קפה',
                 title: 'תפריט מגוון',
                 description: 'קפה איכותי, עוגות טריות וארוחות קלות להורים וילדים'
               },
               {
                 id: '5',
-                image: 'https://images.unsplash.com/photo-1587654780291-39c9404d746b?w=800&h=600&fit=crop',
+                image: '/images/untitled-50.jpg',
                 label: 'חוגים',
                 title: 'פעילויות שבועיות',
                 description: 'חוגים קבועים ופעילויות העשרה לילדים'
               },
               {
                 id: '6',
-                image: 'https://images.unsplash.com/photo-1515488764276-beab7607c1e6?w=800&h=600&fit=crop',
+                image: '/images/untitled-60.jpg',
                 label: 'אירועים',
                 title: 'מקום לכל אירוע',
                 description: 'מתאים לימי הולדת, חגיגות משפחתיות ואירועים מיוחדים'
